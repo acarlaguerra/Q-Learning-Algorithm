c8cb4b9ceca7e691b0b2258215de70d65b6bc55c
carolinaberrafato <85506019+carolinaberrafato@users.noreply.github.com>
2022-10-25 03:23:23 -0300

    bug fixing

--- qlearning.py
+++ qlearning.py
@@ -7,46 +7,46 @@ import random
 
 # UP --------------------------------------------------------- #
 
-# States:                 1     2     3     4     5     6  
-matrix_up = np.matrix([[ 0.1 , 0.1 , 0.8 ,  0  ,  0  ,  0  ],
-                       [ 0.1 , 0.1 ,  0  , 0.8 ,  0  ,  0  ],
-                       [  0  ,  0  , 0.1 , 0.1 , 0.8 ,  0  ],
-                       [  0  ,  0  , 0.1 , 0.1 ,  0  , 0.8 ],
-                       [  0  ,  0  ,  0  ,  0  , 0.9 , 0.1 ],
-                       [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
+# States:                1     2     3     4     5     6  
+matrix_up = np.array([[ 0.1 , 0.1 , 0.8 ,  0  ,  0  ,  0  ],
+                      [ 0.1 , 0.1 ,  0  , 0.8 ,  0  ,  0  ],
+                      [  0  ,  0  , 0.1 , 0.1 , 0.8 ,  0  ],
+                      [  0  ,  0  , 0.1 , 0.1 ,  0  , 0.8 ],
+                      [  0  ,  0  ,  0  ,  0  , 0.9 , 0.1 ],
+                      [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
 
 
 # DOWN ------------------------------------------------------- #
 
-# States:                   1     2     3     4     5     6  
-matrix_down = np.matrix([[ 0.9 , 0.1 ,  0  ,  0  ,  0  ,  0  ],
-                         [ 0.1 , 0.9 ,  0  ,  0  ,  0  ,  0  ],
-                         [ 0.8 ,  0  , 0.1 , 0.1 ,  0  ,  0  ],
-                         [  0  , 0.8 , 0.1 , 0.1 ,  0  ,  0  ],
-                         [  0  ,  0  , 0.8 ,  0  , 0.1 , 0.1 ],
-                         [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
+# States:                  1     2     3     4     5     6  
+matrix_down = np.array([[ 0.9 , 0.1 ,  0  ,  0  ,  0  ,  0  ],
+                        [ 0.1 , 0.9 ,  0  ,  0  ,  0  ,  0  ],
+                        [ 0.8 ,  0  , 0.1 , 0.1 ,  0  ,  0  ],
+                        [  0  , 0.8 , 0.1 , 0.1 ,  0  ,  0  ],
+                        [  0  ,  0  , 0.8 ,  0  , 0.1 , 0.1 ],
+                        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
 
 
 # LEFT ------------------------------------------------------- #
 
-# States:                   1     2     3     4     5     6  
-matrix_left = np.matrix([[ 0.9 ,  0  , 0.1 ,  0  ,  0  ,  0  ],
-                         [ 0.8 , 0.1 ,  0  , 0.1 ,  0  ,  0  ],
-                         [ 0.1 ,  0  , 0.8 ,  0  , 0.1 ,  0  ],
-                         [  0  , 0.1 , 0.8 ,  0  ,  0  , 0.1 ],
-                         [  0  ,  0  ,  0.1  ,  0  , 0.9 , 0 ],
-                         [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
+# States:                  1     2     3     4     5     6  
+matrix_left = np.array([[ 0.9 ,  0  , 0.1 ,  0  ,  0  ,  0  ],
+                        [ 0.8 , 0.1 ,  0  , 0.1 ,  0  ,  0  ],
+                        [ 0.1 ,  0  , 0.8 ,  0  , 0.1 ,  0  ],
+                        [  0  , 0.1 , 0.8 ,  0  ,  0  , 0.1 ],
+                        [  0  ,  0  , 0.1 ,  0  , 0.9 ,  0  ],
+                        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
 
 
 # RIGHT ------------------------------------------------------ #
 
-# States:                    1     2     3     4     5     6  
-matrix_right = np.matrix([[ 0.1 , 0.8 , 0.1 ,  0  ,  0  ,  0  ],
-                          [  0  , 0.9 ,  0  , 0.1 ,  0  ,  0  ],
-                          [ 0.1 ,  0  ,  0  , 0.8 , 0.1 ,  0  ],
-                          [  0  , 0.1 ,  0  , 0.8 ,  0  , 0.1 ],
-                          [  0  ,  0  , 0.1 ,  0  , 0.1 , 0.8 ],
-                          [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
+# States:                   1     2     3     4     5     6  
+matrix_right = np.array([[ 0.1 , 0.8 , 0.1 ,  0  ,  0  ,  0  ],
+                         [  0  , 0.9 ,  0  , 0.1 ,  0  ,  0  ],
+                         [ 0.1 ,  0  ,  0  , 0.8 , 0.1 ,  0  ],
+                         [  0  , 0.1 ,  0  , 0.8 ,  0  , 0.1 ],
+                         [  0  ,  0  , 0.1 ,  0  , 0.1 , 0.8 ],
+                         [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]])
 
 
 # ---------------------- #
@@ -97,16 +97,17 @@ actions_list = ['UP', 'DOWN', 'LEFT', 'RIGHT']
 # ---------------------- #
 #        FUNÇÕES         #
 # ---------------------- #
+
 #função para calcular o próximo estado
-def to_next_state(state, transition_row):
+def to_next_state(transition_row):
     next_list = []
+
     #queremos guardar os valores não nulos da linha da matriz
     for index in range(len(transition_row)):
-        # transition_row = transition_row[0][in]
-        print('t-row ', transition_row[state][index])
-        print('t-row ', len(transition_row))
-        if transition_row[state][index] != 0:
-            next_list.append((transition_row[state][index], index))
+
+        if transition_row[index] != 0:
+            next_list.append((transition_row[index], index))
+
 
     #ordenamos a nossa lista a partir do primeiro elemento de cada tupla
     type = [('t_row', float), ('index', int)]
@@ -121,45 +122,48 @@ def to_next_state(state, transition_row):
     #escolhendo qual valor será usado no next state
     random_num = random.random()
 
+    var = 0
     for k in range(len(cumulative_sum)):
         if cumulative_sum[k] > random_num:
+            var = k
             break
-
-    return(sorted_nlist[k])
+    
+    return(sorted_nlist[var])
 
 
 #função de atualização de Q
 def Q_update(curr_state, action_index, next_state):
-    possible_q = reward_matrix[curr_state] + gamma * max(Q_matrix[next_state])
-    value_q = Q_matrix[curr_state, action_index] + alpha(possible_q - Q_matrix[curr_state, action_index])
+
+    possible_q = reward_matrix[curr_state][action_index] + gamma * max(Q_matrix[next_state])
+    value_q = Q_matrix[curr_state][action_index] + alpha * (possible_q - Q_matrix[curr_state][action_index])
+
     return value_q
 
+# ---------------------- #
+#      INICIALIZAÇÃO     #
+# ---------------------- #
 
-#função de inicialização
 for i in range(25):
     curr_state = 0
     the_end = False
 
     while not the_end:
-        num_rand = random.randint(0,3)
+        random_action = random.randint(0,3)
 
-        if(num_rand == 0):
+        if(random_action == 0):
             transition_state = matrix_up[curr_state]
-        elif(num_rand == 1):
+        elif(random_action == 1):
             transition_state = matrix_down[curr_state]
-        elif(num_rand == 2):
+        elif(random_action == 2):
             transition_state = matrix_left[curr_state]
         else:
             transition_state = matrix_right[curr_state]
 
-        next_state =to_next_state(curr_state, transition_state)
+        next_state = to_next_state(transition_state)
 
-        Q_matrix[curr_state, num_rand] = Q_update(curr_state, num_rand, next_state)
+        Q_matrix[curr_state][random_action] = Q_update(curr_state, random_action, next_state[1])
 
-        curr_state = next_state
+        curr_state = next_state[1]
 
-        if (curr_state == 6):
+        if (curr_state == 5):
             the_end = True
-
-
-
